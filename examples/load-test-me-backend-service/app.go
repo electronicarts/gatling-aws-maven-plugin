73fb30ef5d19a97e7c2b0db5ee26cde8cc928eb2
Ingo Jaeckel <ijaeckel@ea.com>
2016-06-05 16:23:22 -0700

    changed from standard to flexible GAE environment
    use 1..100 instances with 2 cores instead of GAE's default

--- examples/load-test-me-backend-service/app.go
+++ examples/load-test-me-backend-service/app.go
@@ -1,15 +1,21 @@
 // Copyright (C) 2016 Electronic Arts Inc. All rights reserved.
-package ping 
+package main
 
 import (
-    "fmt"
-    "net/http"
+	"fmt"
+	"log"
+	"net/http"
 )
 
 func init() {
-    http.HandleFunc("/", handler)
 }
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprint(w, "pong")
+	fmt.Fprint(w, "pong")
+}
+
+func main() {
+	http.HandleFunc("/", handler)
+	log.Print("Listening on port 8080")
+	log.Fatal(http.ListenAndServe(":8080", nil))
 }
---
+++ examples/load-test-me-backend-service/deploy.sh
@@ -0,0 +1,3 @@
+#!/bin/sh
+# Copyright (C) 2016 Electronic Arts Inc. All rights reserved.
+aedeploy gcloud preview app deploy --project load-test-me --version 1
