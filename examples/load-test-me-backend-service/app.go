8ef0d715cafff720ceee364d6c2cf22ac28bd02b
Ingo Jaeckel <ingo.jaeckel@gmail.com>
2016-08-30 22:41:03 -0700

    reverted back to GAE standard environment to reduce min costs to 0

--- examples/load-test-me-backend-service/app.go
+++ examples/load-test-me-backend-service/app.go
@@ -3,19 +3,12 @@ package main
 
 import (
 	"fmt"
-	"log"
 	"net/http"
 )
 
 func init() {
-}
-
-func handler(w http.ResponseWriter, r *http.Request) {
+    http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
 	fmt.Fprint(w, "pong")
+    })
 }
 
-func main() {
-	http.HandleFunc("/", handler)
-	log.Print("Listening on port 8080")
-	log.Fatal(http.ListenAndServe(":8080", nil))
-}
